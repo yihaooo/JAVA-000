f4841ca9dd7d5013701535f4f8d169ef4c5970b5
yihao <54434872@qq.com>
2020-10-21 22:18:22 +0800

    第一周作业

---
+++ Week_01/HelloClassloader.java
@@ -0,0 +1,45 @@
+package demo;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+
+public class HelloClassloader extends ClassLoader {
+    public static void main(String[] args) {
+        try {
+            Class<?> aClass = new HelloClassloader().findClass("Hello");
+            Object obj = aClass.newInstance();
+            Method method = aClass.getMethod("hello");
+            method.invoke(obj);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        File f = new File(this.getClass().getResource("/Hello.xlass").getPath());
+        int length = (int)f.length();
+        byte[] bytes = new byte[length];
+        try {
+            new FileInputStream(f).read(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return super.findClass(name);
+        }
+        for (int i = 0; i < bytes.length; ++i) {
+            bytes[i] = (byte)(255 - bytes[i]);
+        }
+        return defineClass(name, bytes, 0, bytes.length);
+    }
+}
